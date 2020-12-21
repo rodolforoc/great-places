26286aac2d8e015614920cfa49b760fe15ba03f3
rodolforoc <rodolforoc97@gmail.com>
2020-12-21 12:28:27 -0300

    add sqlite to store data

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,25 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
+
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
@@ -28,6 +44,11 @@ class GreatPlaces with ChangeNotifier {
     );
 
     _items.add(newPlace);
+    DbUtil.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
     notifyListeners();
   }
 }
