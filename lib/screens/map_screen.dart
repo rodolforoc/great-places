9f0534f741f6323eaad440f6ed3050c8a3a978c8
rodolforoc <rodolforoc97@gmail.com>
2020-12-21 23:53:18 -0300

    select location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -4,12 +4,14 @@ import 'package:great_places/models/place.dart';
 
 class MapScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
+  final bool isReadOnly;
 
   MapScreen({
     this.initialLocation = const PlaceLocation(
       latitude: 37.419857,
       longitude: -122.078827,
     ),
+    this.isReadOnly = false,
   });
 
   @override
@@ -17,11 +19,30 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedPosition;
+
+  void _selectPosition(LatLng position) {
+    setState(() {
+      _pickedPosition = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Selecione...'),
+        actions: [
+          if (!widget.isReadOnly)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedPosition == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedPosition);
+                    },
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
@@ -31,6 +52,15 @@ class _MapScreenState extends State<MapScreen> {
           ),
           zoom: 13,
         ),
+        onTap: widget.isReadOnly ? null : _selectPosition,
+        markers: _pickedPosition == null
+            ? null
+            : {
+                Marker(
+                  markerId: MarkerId('p1'),
+                  position: _pickedPosition,
+                ),
+              },
       ),
     );
   }
